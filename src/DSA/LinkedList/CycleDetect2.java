14e398c17b253636bc0f7888bf3ded302e03332c
virendra <vir93500@yahoo.com>
2026-07-16 01:07:32 +0530

    Linked list DSA

---
+++ src/DSA/LinkedList/CycleDetect2.java
@@ -0,0 +1,30 @@
+package DSA.LinkedList;
+
+public class CycleDetect2 {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean flag = false;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow==fast){
+                flag=true;
+                break;
+            }
+        }
+        if(!flag)
+            return null;
+
+        slow = head;
+        while(slow!=fast){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        return slow;
+
+
+    }
+}
